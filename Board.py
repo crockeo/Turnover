a2e122dea93d25cc3ee6a1238594d5deb6f5739c
Crockeo <crockeo@gmail.com>
2012-09-13 09:46:50 -0400

    Updated Board and Player

--- Board.py
+++ Board.py
@@ -14,10 +14,19 @@ class Board:
 	def draw(self): print str(self)
 	def convert(self, x, y): return (y * self.getWidth()) + x
 
+	# Flipping a tile
+	def flip(self, x, y):
+		if self.getTile(x, y) == Data.UNACTIVATED:
+			self.setTile(x, y, Data.ACTIVATED)
+		elif self.getTile(x, y) == Data.ACTIVATED:
+			self.setTile(x, y, Data.UNACTIVATAED)
+
+	# Accessors
 	def getWidth(self): return self._width
 	def getHeight(self): return self._height
 	def getTiles(self): return self._tiles
 	def getTile(self, x, y): return self._tiles[self.convert(x y)]
 
+	# Mutators
 	def setAll(self, value): self._tiles = [value]*(self.getWidth() * self.getHeight())
 	def setTile(self, x, y, value): self._tiles[self.convert(x, y)] = value
--- Player.py
+++ Player.py
@@ -7,3 +7,51 @@ class Player:
 
 		self.setUnder(board.getTile(self.getX(), self.getY()))
 		board.setTile(self.getX(), self.getY(), Data.PLAYER)
+
+	# Updating the player
+	def update(self, read, board):
+		if read == 'w':
+			newPos = self.wouldMoveTo(0, -1)
+		elif read == 's':
+			newPos = self.wouldMoveTo(0,  1)
+		elif read == 'a':
+			newPos = self.wouldMoveTo(-1, 0)
+		elif read == 'd':
+			newPos = self.wouldMoveTo( 1, 0)
+
+		# Collision
+		if (newPos[0] < 0) or (newPos[0] > board.getWidth() - 1):
+			return
+		elif (newPos[1] < 0) or (newPos[1] > board.getHeight() - 1):
+			return
+		elif board.getTile(newPos[0], newPos[1]) == Data.WALL:
+			return
+
+		# If all is well, just move it!
+		
+
+	# Moving
+	def move(self, dX, dY):
+		self.setX(self.getX() + dX)
+		self.setY(self.getY() + dY)
+
+	def wouldMoveTo(self, dX, dY): return (self.getX() + dX, self.getY() + dY)
+
+	# Accessors
+	def getPosition(self): return (self.getX(), self.getY())
+	def getX(self): return self._x
+	def getY(self): return self._y
+	def getUnder(self): return self._under
+
+	# Mutators
+	def setPosition(self, x, y):
+		self.setX(x)
+		self.setY(y)
+
+	def moveTo(self, x, y):
+		self.setX(x)
+		self.setY(y)
+
+	def setX(self, x): self._x = x
+	def setY(self, y): self._y = y
+	def setUnder(self, under): self._under = under
